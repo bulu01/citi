e6682258f5f7fc1ed7457554aeab82e0ee75ec1b
bulu <bulu@localhost>
2018-08-27 20:13:45 +0530

    1st commit

---
+++ abc.java
@@ -0,0 +1,11 @@
+public class java{
+	public static void main(string[]args){
+		double[]arr = {19,12.89.200,13};
+		double total =0;
+		for(int =0; i<arr.length;i++){
+			total = total + arr[i];
+		}
+		double average = total / arr.length;
+		system.out.format ("avg is: %3f" , average);
+	}
+
